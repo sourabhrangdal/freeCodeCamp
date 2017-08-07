c515235eadc5e4a93dce36f6244d37af8d069d7c
Sourabh Rangdal <sourabh.rangdal@gmail.com>
2017-08-07 18:05:13 +0530

    trying with array

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -2,12 +2,14 @@ function convertToRoman(num) {
     
     var decimal = [ 1, 4, 5, 9, 10, 40, 50, 100, 400, 500, 1000];
     var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M',];
-    var deciToRom = '';
+    // var deciToRom = '';
+    var deciToRom = [];
     for(var i = 0; i < decimal; i++){
         while(decimal[i] <= num){
             // deciToRom = deciToRom + roman[i];
-            deciToRom += roman[i];
-            deciToRom = deciToRom - decimal[i];
+            // deciToRom += roman[i];
+            // deciToRom = deciToRom - decimal[i];
+            deciToRom.push(roman[i]);
         }
     }
   return deciToRom;
