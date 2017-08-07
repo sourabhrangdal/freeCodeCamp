d7308ad993fa2533b7b8560ab177192e54f4caea
Sourabh Rangdal <sourabh.rangdal@gmail.com>
2017-08-07 17:48:25 +0530

    decimal output is string?

---
+++ romanNumeralConverter.js
@@ -0,0 +1,13 @@
+function convertToRoman(num) {
+    
+    var decimal = [ 1, 4, 5, 9, 10, 40, 50, 100, 400, 500, 1000];
+    var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M',];
+    var deciToRom = '';
+    for(var i = 0; i < decimal; i++){
+        while(decimal[i] < num){
+            deciToRom = deciToRom + roman[i];
+        }
+    }
+  return deciToRom;
+}
+convertToRoman(36);
