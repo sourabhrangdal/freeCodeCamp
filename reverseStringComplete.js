13549d8fa53df7b608c79020dc9500d1867a9f82
Sourabh Rangdal <sourabh.rangdal@gmail.com>
2017-08-08 19:03:15 +0530

    reverseString attemp 2

--- reverseStringComplete.js
+++ reverseStringComplete.js
@@ -1,4 +1,19 @@
 
+// 2
+function reverseString(str){
+    var splitStr = str.split('');
+    // ('s','o','u','r','a','b','h')
+
+    var reverseStr = splitStr.reverse();
+    // ('h','b','a','r','u','o','s')
+
+    var joinStr = reverseStr.join('');
+    // ('hbaruos')
+
+    return joinStr;
+}
+reverseString("Reverse the string");
+
 // 1
 function reverseString(str){
     return str.split('').reverse().join('');
